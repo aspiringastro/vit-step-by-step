9a4de2c919bcfa9cbabccc10fb4b7310f8d6b786
Aspiring Astro <104707961+aspiringastro@users.noreply.github.com>
2023-02-05 00:20:04 -0800

    interim checkin - position emb

--- dataset/cifar10.py
+++ dataset/cifar10.py
@@ -1,37 +1,68 @@
 from torch.utils.data import DataLoader
 from torchvision import transforms as T
 from torchvision.datasets import CIFAR10
-
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
 
 class CIFAR10DataSet():
-    def __init__(self, data_dir="data/cifar10"):
+    def __init__(self, data_dir="data/cifar10", train_val_split=0.8):
         self.data_dir = data_dir
         self.dataset = CIFAR10(root=self.data_dir, download=True)
         self.mean = (0.485, 0.456, 0.406)
         self.std = (0.229, 0.224, 0.225)
-        self.normalize = T.Normalize(mean=self.mean, std=self.std)
-        self.denormalize = T.Normalize(
-            mean=(-self.mean[0]/self.std[0], -self.mean[1]/self.std[1], -self.mean[2]/self.std[2]),
-            std=(1/self.std[0], 1/self.std[1], 1/self.std[2]),
-            )
+        self.train_val_split = train_val_split
 
-    def  train_dataloader(self, batch_size=32, resize=224, p=0.5, mean=None,std=None, num_workers=4, shuffle=True):
+    def  train_dataloader(self, batch_size=32, resize=32, p=0.5, mean=(0.485, 0.456, 0.406),std=(0.229, 0.224, 0.225), num_workers=4):
         tf = T.Compose([
                 T.RandomResizedCrop(size=resize),
                 T.RandomHorizontalFlip(p=p),
                 T.RandomVerticalFlip(p=p),
                 T.ToTensor(),
-                self.normalize if self.mean == None and self.std == None else T.Normalize(mean, std),
+                T.Normalize(mean, std),
             ]
         )
         ds = CIFAR10(root=self.data_dir, train=True, transform=tf)
-        dl = DataLoader(ds,batch_size=batch_size, num_workers=num_workers,shuffle=shuffle, drop_last=True)
+        num_train = len(ds)
+        indices = list(range(num_train))
+        split = int(np.floor(self.train_val_split * num_train))
+        train_sampler = SubsetRandomSampler(indices[split:])
+        dl = DataLoader(
+            ds,
+            batch_size=batch_size, 
+            num_workers=num_workers, 
+            sampler=train_sampler, 
+            drop_last=True
+            )
         return dl
     
-    def test_dataloader(self, batch_size=32, mean=None ,std=None, num_workers=4):
+    def  val_dataloader(self, batch_size=32, resize=32, p=0.5, mean=(0.485, 0.456, 0.406),std=(0.229, 0.224, 0.225), num_workers=4):
+        tf = T.Compose([
+                T.RandomResizedCrop(size=resize),
+                T.RandomHorizontalFlip(p=p),
+                T.RandomVerticalFlip(p=p),
+                T.ToTensor(),
+                T.Normalize(mean, std),
+            ]
+        )
+        ds = CIFAR10(root=self.data_dir, train=True, transform=tf)
+        num_train = len(ds)
+        indices = list(range(num_train))
+        split = int(np.floor(self.train_val_split * num_train))
+        val_sampler = SubsetRandomSampler(indices[:split])
+        dl = DataLoader(
+            ds,
+            batch_size=batch_size, 
+            num_workers=num_workers, 
+            sampler=val_sampler, 
+            drop_last=True
+            )
+        return dl
+
+    
+    def test_dataloader(self, batch_size=32, mean=(0.485, 0.456, 0.406) ,std=(0.229, 0.224, 0.225), num_workers=4):
         tf = T.Compose([
             T.ToTensor(),
-            self.normalize if self.mean == None and self.std == None else T.Normalize(mean, std),
+            T.Normalize(mean, std),
             ]
         )
         ds = CIFAR10(root=self.data_dir, train=False, transform=tf)
@@ -40,4 +71,9 @@ class CIFAR10DataSet():
     
     def get_batch(self, dataloader):
         return next(iter(dataloader))
+    
+    def get_classes(self):
+        classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+        return classes
 
